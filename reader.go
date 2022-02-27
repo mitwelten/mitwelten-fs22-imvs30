32c58917cd561426ad4c2010f2e284cf686c8725
Tobias Kunz <tobias.kunz@students.fhnw.ch>
2022-02-27 13:54:47 +0100

    Refactoring

--- reader.go
+++ reader.go
@@ -1,3 +1,4 @@
+//https://go101.org/article/channel-use-cases.html
 package main
 
 import (
@@ -8,6 +9,11 @@ import (
 	"sync"
 	"time"
 )
+
+type Frame struct {
+	header []byte
+	body   []byte
+}
 
 var delim = []byte("--boundarydonotcross\r\n")
 var frame_start = []byte("\xff\xd8\xff\xe0")
@@ -28,63 +34,69 @@ func read_til_boundry(r *bufio.Reader) (data []byte) {
 	}
 }
 
-func parse_frame(data []byte) (header []byte, body []byte) {
+func parse_frame(data []byte) (frame Frame) {
 	for i := 0; i < len(data); i++ {
 		if bytes.Compare(data[i:i+4], frame_start) == 0 {
-			return data[:i], data[i:]
+			return Frame{data[:i], data[i:]}
 		}
 	}
 	panic("invalid frame")
 }
 
-func start_socket(port string, channel chan []byte) {
+func start_socket(port string) (reader *bufio.Reader) {
 	conn, err := net.Dial("tcp", "127.0.0.1:"+port)
 	if err != nil {
 		panic("Socket error")
 	}
 	conn.Write([]byte("GET /?action=stream HTTP/1.1\r\nHost:%s\r\n\r\n"))
 
-	reader := bufio.NewReader(conn)
-
-	//header
-	_ = read_til_boundry(reader)
-
-	for {
-		var data = read_til_boundry(reader)
-		var _, frame = parse_frame(data)
-		//os.WriteFile("./out_" +port+ ".jpg", frame, 0644)
-		channel <- frame
-	}
+	return bufio.NewReader(conn)
 
 }
 
-func sink(channels []chan []byte) {
-	for {
-		for _, channel := range channels {
-			frame := <-channel
-			os.WriteFile("./out_.jpg", frame, 0644)
-		time.Sleep(250 * time.Millisecond)
+func source(reader *bufio.Reader) chan Frame {
+	var source = make(chan Frame)
+	go func() {
+		//header
+		_ = read_til_boundry(reader)
+		for {
+			var data = read_til_boundry(reader)
+			var frame = parse_frame(data)
+			//os.WriteFile("./out_" +port+ ".jpg", frame, 0644)
+			source <- frame
 		}
-	}
+	}()
+
+	return source
+}
+
+func sink(sources []chan Frame) {
+	go func() {
+		for {
+			for _, source := range sources {
+				var frame = <-source
+				os.WriteFile("./out_.jpg", frame.body, 0644)
+				time.Sleep(250 * time.Millisecond)
+			}
+		}
+	}()
 }
 
 func main() {
 	var args = os.Args[1:]
-
-	var channels []chan []byte
-	//for i := range args {
-	//  channels = append(channels, make(chan []byte))
-
 	var wg sync.WaitGroup
+
+	var channels []chan Frame
+
 	for _, port := range args {
-		var channel = make(chan []byte)
-		//channels[i] = channel
-		channels = append(channels, channel)
 		wg.Add(1)
-		go start_socket(port, channel)
+		var reader = start_socket(port)
+		var channel = source(reader)
+		channels = append(channels, channel)
 	}
 
-	go sink(channels)
+	wg.Add(1)
+	sink(channels)
 
 	wg.Wait()
 }
