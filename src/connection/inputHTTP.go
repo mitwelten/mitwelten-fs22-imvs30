afa0410ca59c8c2e820c5ceb85ef1819370d86d6
m-bug <marc.bugmann@outlook.com>
2022-03-16 13:24:13 +0100

    adds args handling

--- src/connection/inputHTTP.go
+++ src/connection/inputHTTP.go
@@ -3,6 +3,7 @@ package connection
 import (
 	"bytes"
 	"errors"
+	"fmt"
 	"io"
 	"mjpeg_multiplexer/src/mjpeg"
 	"net"
@@ -14,6 +15,7 @@ type InputHTTP struct {
 }
 
 func NewInputHTTP(url string) (source InputHTTP) {
+	fmt.Println(url)
 	conn, err := net.Dial("tcp", url)
 	if err != nil {
 		panic("Socket error")
--- src/main.go
+++ src/main.go
@@ -6,11 +6,8 @@ import (
 	"mjpeg_multiplexer/src/aggregator"
 	"mjpeg_multiplexer/src/connection"
 	"mjpeg_multiplexer/src/mjpeg"
-	"strings"
-)
-
-import (
 	"os"
+	"strings"
 	"sync"
 )
 
@@ -45,40 +42,56 @@ func run() {
 	wg.Wait()
 }
 
+func parseInput(inputStr string) {
+	InputLocations = strings.Split(inputStr, " ")
+}
+
 func parseArgs() {
-	fileCmd := flag.NewFlagSet("file", flag.ExitOnError)
-	fileName := fileCmd.String("name", "", "filename")
+	//---Define all various flags---
+	inputPtr := flag.String("input", "", "Use Input flag to determine input streams")
+	outputPtr := flag.String("output", "", "Use Out flag to determine output modus") // file oder stream
+	outputFileNamePtr := flag.String("output_filename", "", "")
+	outputStreamPortPtr := flag.String("output_port", "", "port for output stream")
+	methodPtr := flag.String("method", "", "How will the out be mixed?") // grid, combine etc.
 
-	streamCmd := flag.NewFlagSet("stream", flag.ExitOnError)
-	streamPort := streamCmd.String("port", "", "port address")
+	//---parse the command line into the defined flags---
+	flag.Parse()
 
-	inputCmd := flag.NewFlagSet("input", flag.ExitOnError)
-	inputLocations := inputCmd.String("locations", "", "string list of locations")
-
-	if len(os.Args) < 2 {
-		fmt.Println("expected 'file' or 'stream' subcommands")
-		os.Exit(1)
+	// first validation
+	// check if at least all three mandatory parameters are present
+	if len(*inputPtr) == 0 || len(*outputPtr) == 0 || len(*methodPtr) == 0 {
+		fmt.Println("expected at least '-input' '-output' and '-method' arguments")
+		os.Exit(2)
 	}
-
-	switch os.Args[1] {
-	case "file":
-		fileCmd.Parse(os.Args[2:4])
-		Output = connection.NewOutputFile(*fileName)
-	case "stream":
-		streamCmd.Parse(os.Args[2:4])
-		var err error
-		Output, err = connection.NewOutputHTTP(*streamPort)
-		if err != nil {
-			println(err.Error())
-			panic("Could not open output socket, aborting...")
+	// stream
+	if strings.Compare(*outputPtr, "stream") == 0 {
+		if len(*outputStreamPortPtr) == 0 {
+			fmt.Println("-output 'stream' only valid in combination with -output_port ")
+			os.Exit(3)
+		} else {
+			Output = connection.NewOutputFile(*outputStreamPortPtr)
 		}
-	default:
-		fmt.Println("expected 'file' or 'stream' subcommands")
-		os.Exit(1)
+		// file
+	} else if strings.Compare(*outputPtr, "file") == 0 {
+		if len(*outputFileNamePtr) == 0 {
+			fmt.Println("-output 'file' only valid in combination with -output_filename ")
+			os.Exit(4)
+		} else {
+			Output = connection.NewOutputFile(*outputFileNamePtr)
+		}
+	} else {
+		fmt.Println("invalid output argument: -output argument '" + *outputPtr + "' not valid. Use -output 'stream' or -output 'file'")
+		os.Exit(5)
 	}
 
-	inputCmd.Parse(os.Args[5:])
-	InputLocations = strings.Split(*inputLocations, " ")
+	// input parsing
+	parseInput(*inputPtr)
+
+	fmt.Println(*inputPtr)
+	fmt.Println(*outputPtr)
+	fmt.Println(*outputFileNamePtr)
+	fmt.Println(*outputStreamPortPtr)
+	fmt.Println(*methodPtr)
 
 }
 func main() {
