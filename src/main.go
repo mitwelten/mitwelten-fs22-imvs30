c593a39c7ff322f1e10b95b71e725bd734e04ab5
m-bug <marc.bugmann@outlook.com>
2022-03-30 14:44:15 +0200

    adds logger

--- src/main.go
+++ src/main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"fmt"
+	"io"
+	"log"
 	"mjpeg_multiplexer/src/args"
 	"mjpeg_multiplexer/src/communication"
 	"mjpeg_multiplexer/src/connection"
@@ -9,9 +10,20 @@ import (
 	"sync"
 )
 
-// as per https://docs.fileformat.com/image/jpeg/
-
 var Config args.MultiplexerConfig
+
+const logFile string = "multiplexer.log"
+
+func setupLog() {
+	// log setup
+	file, err := os.OpenFile(logFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	mw := io.MultiWriter(os.Stdout, file)
+	log.SetOutput(mw)
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+}
 
 func run() {
 	var wg sync.WaitGroup
@@ -37,13 +49,15 @@ func run() {
 }
 
 func main() {
+	setupLog()
+	log.Println("Running the MJPEG-multiFLEXer")
+
+	log.Println("parsing args...")
 	// loop over all arguments by index and value
 	for i, arg := range os.Args {
 		// print index and value
-		fmt.Println("item", i, "is", arg)
+		log.Println("item", i, "is", arg)
 	}
-
-	println("Running the MJPEG-multiFLEXer")
 
 	c, err := args.ParseArgs(os.Args)
 	if err != nil {
