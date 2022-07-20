19d5336cb6352d4ce1fe3d4e4542d4bd6e592883
Tobias Kunz <tobias.kunz@students.fhnw.ch>
2022-07-20 09:48:45 +0200

    Added logs to authentication parsing

--- src/utils/Authentication.go
+++ src/utils/Authentication.go
@@ -36,14 +36,18 @@ func ParseAuthenticationFile() {
 		log.Fatalf("Can't parse authentication file json: %v\n", err.Error())
 	}
 
+	counter := 0
 	for _, entry := range data {
 		auth := entry
 		payload := base64.StdEncoding.EncodeToString([]byte(auth.Username + ":" + auth.Password))
 
 		index := findInputConfigIndex(auth.Url)
 		if index == -1 {
+			log.Printf("Warning: Authentication entry with URL %v does not match any input\n", entry.Url)
 			continue
 		}
 		global.Config.InputConfigs[index].Authentication = payload
+		counter++
 	}
+	log.Printf("Parsed %v authentication entries\n", counter)
 }
